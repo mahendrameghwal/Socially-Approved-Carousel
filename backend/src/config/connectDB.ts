023c93c2ce18c634941198e2c14ac37e439c93dc
mahendrameghwal <solankirakesh895@gmail.com>
2026-06-10 23:51:19 +0530

    refactor: remove unused blog services and Google Drive integration

--- backend/src/config/connectDB.ts
+++ backend/src/config/connectDB.ts
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-import { autoScheduleJobs } from "../controllers/blog.controller";
+
 
 const connectDB = async () => {
 const MongoURL: string |undefined =  process.env.MONGO_URI!;
@@ -7,7 +7,6 @@ console.log(MongoURL)
   try {
     await mongoose.connect(MongoURL);
     console.log("MongoDB connected");
-    autoScheduleJobs()
 
   } catch (error) {
     console.error("MongoDB connection failed:", error);
--- backend/src/services/blog.services.ts
+++
@@ -1,5 +0,0 @@
-import Blog from '../models/blog.model';
-
-export const getPublishedBlogs = async () => {
-  return Blog.find({ status: 'published' }).populate('category author');
-};
